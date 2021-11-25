ecc1d21b312b464b5864f0aade86211bdb8ec842
V3ntus <29584664+V3ntus@users.noreply.github.com>
2021-11-24 23:14:35 -0600

    Merge branch 'main' of https://github.com/V3ntus/darvester

--- run.py
+++ run.py
@@ -17,13 +17,6 @@ from cfg import TOKEN
 
 import selfcord as discord
 from selfcord.ext import commands
-import sqlite3, datetime
-
-con = sqlite3.connect("harvested.db")
-cur = con.cursor()
-
-# Create initial table if it doesn't exist
-cur.execute('''CREATE TABLE IF NOT EXISTS users (name text, user_id integer UNIQUE, public_flags text)''')
 
 client = commands.Bot(command_prefix=",",
     case_insensitive=True,
@@ -34,24 +27,9 @@ client = commands.Bot(command_prefix=",",
 async def on_ready():
     print("Logged in as {0.user}".format(client))
     print("Starting guild ID dump...")
-    for guildid in [guild.id for guild in client.guilds]:
+    listofguilds = [guild.id for guild in client.guilds]
+    for guildid in listofguilds:
         guild = client.get_guild(guildid)
-        for member in guild.members:
-            if not member.bot or not member.system:
-                # if member isn't a bot or a Discord system user
-                try:
-                    print(f"Inserting {member.name}, {member.id}")
-                    cur.execute("INSERT INTO users VALUES (?, ?, ?)",
-                        (member.name,
-                            member.id,
-                            member.public_flags.all() # instead of all(), use value?
-                        )
-                            # int(member.created_at.timestamp()),
-                    )
-                    con.commit()
-                except sqlite3.IntegrityError:
-                    # try ALTER to insert guild_id
-                    print(f"Already exists: {member.id} for {member.name}")
-        
+        print(guild.members)
 
 client.run(TOKEN)
