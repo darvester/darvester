19cab09507ef31301bcd00c956c6211d664f2317
V3ntus <29584664+V3ntus@users.noreply.github.com>
2021-11-26 23:11:52 -0600

    SQLite JSON init

--- run.py
+++ run.py
@@ -17,6 +17,36 @@ from cfg import TOKEN
 
 import selfcord as discord
 from selfcord.ext import commands
+import sqlite3, json
+from pprint import pprint
+
+class sqlitenosql:
+    def __init__(self, f):
+        self.db = sqlite3.connect(f)
+        self.cur = self.db.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS main(data TEXT UNIQUE, id INTEGER UNIQUE);")
+
+    def close(self):
+        self.db.commit()
+        self.db.close()
+
+    def addrow(self, d, i):
+        try:
+            self.db.execute("INSERT INTO main VALUES (?, ?);", 
+                (json.dumps(d), i,)
+            )
+        except sqlite3.IntegrityError:
+            print(f"Already exists: {i}")
+
+    def find(self, query):
+        for k, v in query.items():
+            if isinstance(v, str):
+                query[k] = f"'{v}'"
+        q = ' AND '.join(f" json_extract(data, '$.{k}') = {v}" for k, v in query.items())
+        for r in self.db.execute(f"SELECT * FROM main WHERE {q}"):
+            yield r[0]
+
+db = sqlitenosql("harvested.db")
 
 client = commands.Bot(command_prefix=",",
     case_insensitive=True,
@@ -30,6 +60,14 @@ async def on_ready():
     listofguilds = [guild.id for guild in client.guilds]
     for guildid in listofguilds:
         guild = client.get_guild(guildid)
-        print(guild.members)
+        for member in guild.members:
+            if not member.bot and not member.system:
+                post = {'name'  : member.name,
+                        'id'    : member.id,
+                        'public_flags': member.public_flags.all()}
+                print(f"Inserting {member.id}:")
+                pprint(post)
+                db.addrow(post, member.id)
+        db.close()
 
 client.run(TOKEN)
