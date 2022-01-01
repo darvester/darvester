be32baa6277cc33d8232f9eeff6c725f2afea222
V3ntus <29584664+V3ntus@users.noreply.github.com>
2021-12-31 21:07:00 -0600

    add: ,select command module

---
+++ commands/select_cmd.py
@@ -0,0 +1,54 @@
+import datetime
+import traceback
+import asyncio
+from src import logutil
+
+logger = logutil.initLogger("select_cmd")
+
+
+async def _main(message, db):
+    logger.info('"%s" - initiated a select command', message.author.name)
+    if len(message.content) > 7:
+        try:
+            data = db.find(
+                message.content[7:].lstrip().rstrip(),
+                "users",
+            )
+            if data:
+                _connected_accounts = "".join(
+                    f"- {i['type']} - {i['name']}\n"
+                    for i in data["connected_accounts"]
+                )
+
+                _message = f"""
+    Name: `{data["name"]}#{data["discriminator"]}`
+    Bio: ```{data["bio"]}```
+    Mutual Guilds: `{data["mutual_guilds"]["guilds"]}`
+    Avatar: {data["avatar_url"]}
+    Account Created At: `{datetime.fromtimestamp(data["created_at"])}`
+    Connected Accounts:
+    ```
+    {_connected_accounts}
+    ```
+    """
+                logger.info(
+                    'Found "%s" requested by user "%s"' %
+                    (data["name"], message.author.name),
+                )
+                await message.channel.send(_message)
+            else:
+                await message.channel.send(
+                    "Query returned empty. User not\
+    found"
+                )
+        except Exception as e:  # noqa
+            logger.warning(",select triggered exception")
+            traceback.print_exc()
+            await message.channel.send(
+                "Something wrong happened:```py \
+    %s \
+    ```"
+                % (e)
+            )
+            await asyncio.sleep(2)
+            await message.channel.send("```%s```" % traceback.format_exc())
