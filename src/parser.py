c81902cfb3111d2b5394472f5cd18a384fae22a9
V3ntus <29584664+V3ntus@users.noreply.github.com>
2022-01-01 13:58:26 -0600

    fix: github parsing not formatted properly

--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,19 @@
 import aiohttp
+from src import logutil
+logger = logutil.initLogger("parser")
 
 
 class ConnectedAccounts:
     async def _parse_github(self):
+        logger.debug("Attempting to parse github...")
         try:
-            _url = "https://api.github.com/user/{self._id}"
+            _url = f"https://api.github.com/user/{self._id}"
             async with aiohttp.ClientSession() as _session:
                 async with _session.get(_url) as _resp:
                     _resp = await _resp.json()
                     return _resp["html_url"]
-        except:  # noqa
+        except Exception:  # noqa
+            logger.warning("Falling back to inaccurate Github URL", exc_info=1)
             return f"https://github.com/{self._name}"
 
     def __init__(self, *args, **kwargs) -> None:
@@ -21,6 +25,12 @@ class ConnectedAccounts:
         self._type = kwargs.pop('type', None)
         self._id = kwargs.pop('id', None)
         self._name = kwargs.pop('name', None)
+
+        logger.debug("Parsing type: %s, id: %s, name: %s" % (
+            self._type,
+            self._id,
+            self._name
+        ))
 
         if self._type == "battlenet":
             return self._name  # no api(?)
@@ -42,3 +52,5 @@ class ConnectedAccounts:
             return self._name   # no api(?)
         elif self._type == "youtube":
             return f"https://youtube.com/channel/{self._id}"
+        else:
+            return self._name
