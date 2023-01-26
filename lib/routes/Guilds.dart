bcd9b9c3af191ef4a24ffc4d332d2a0dc921473f
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-01-26 12:42:17 -0600

    feat: wip: invalid database path warning

--- lib/routes/Guilds.dart
+++ lib/routes/Guilds.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 // Util
 import '../util.dart';
 
 // Components
 import '../components/MainDrawer.dart';
+
+// Routes
+import 'Settings.dart';
 
 class Guilds extends StatefulWidget {
   const Guilds({Key? key}) : super(key: key);
@@ -16,11 +20,34 @@ class Guilds extends StatefulWidget {
 class _GuildsState extends State<Guilds> {
   DarvesterDB? db;
   bool isLoading = true;
-  
+
   @override
   void initState() {
     super.initState();
-
+    Preferences.instance.getString("databasePath").then((value) {
+      if (value.isNotEmpty) {
+        db = DarvesterDB(value);
+      } else {
+        showDialog(
+            context: context,
+            builder: (BuildContext builder) {
+              return AlertDialog(
+                  title: const Text("Database path empty"),
+                  content: const Text(
+                      "Database path is not set. Please set in Settings"),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () => context.go("/"),
+                        child: const Text("Go back")),
+                    TextButton(
+                        onPressed: () => Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => const Settings())
+                        ),
+                        child: const Text("Take me there")),
+                  ]);
+            });
+      }
+    });
   }
 
   @override
