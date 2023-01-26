16b000cc66d79f8625fbf1936f7f443349eef971
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-01-26 12:22:18 -0600

    feat: wip: convert Guilds to stateful widget

--- lib/routes/Guilds.dart
+++ lib/routes/Guilds.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+// Util
+import '../util.dart';
+
 // Components
 import '../components/MainDrawer.dart';
 
-class Guilds extends StatelessWidget {
+class Guilds extends StatefulWidget {
   const Guilds({Key? key}) : super(key: key);
+
+  @override
+  State<Guilds> createState() => _GuildsState();
+}
+
+class _GuildsState extends State<Guilds> {
+  DarvesterDB? db;
+  bool isLoading = true;
+  
+  @override
+  void initState() {
+    super.initState();
+
+  }
 
   @override
   Widget build(BuildContext context) {
