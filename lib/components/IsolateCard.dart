b00bfdd3a5d5f1166e45484fe50d86a14146e219
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-03-30 12:15:53 -0500

    feat: polish up IsolateCard and add action buttons

--- lib/components/IsolateCard.dart
+++ lib/components/IsolateCard.dart
@@ -1,4 +1,5 @@
 import 'package:crypto/crypto.dart';
+import 'package:darvester/darvester/harvester_isolate.dart';
 import 'package:flutter/material.dart';
 
 import '../darvester/isolate_set.dart';
@@ -33,56 +34,88 @@ class _IsolateCardState extends State<IsolateCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      constraints: const BoxConstraints(
+        maxHeight: 520,
+      ),
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         color: const Color(0xff444444),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(widget.digest.toString()),
+          Text(
+            style: const TextStyle(
+              fontSize: 12,
+              color: Color(0xffaaaaaa),
+            ),
+            widget.digest.toString().substring(0, 8),
           ),
           Row(
             children: [
-              Expanded(
-                child: AnimatedSize(
-                  curve: Curves.easeInOut,
-                  duration: const Duration(milliseconds: 100),
-                  child: Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: const Color(0xff333333),
+              Padding(
+                padding: const EdgeInsets.all(2),
+                child: AnimatedOpacity(
+                  opacity: 1,
+                  duration: const Duration(milliseconds: 500),
+                  child: Text(
+                    style: const TextStyle(
+                      fontSize: 24,
                     ),
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxHeight: 410,
-                      ),
-                      child: SingleChildScrollView(
-                        reverse: true,
-                        child: SelectableText(
-                          style: const TextStyle(
-                            fontFamily: "Courier New",
-                          ),
-                          // filter for log messages in the message queue and print those out to this console display
-                          HarvesterIsolateSet.instance
-                                  .get(widget.digest)
-                                  ?.messageQueue
-                                  .where((message) {
-                                    return message.type == HarvesterIsolateMessageType.log;
-                                  })
-                                  .map((e) => e.data.toString())
-                                  .join("\n") ??
-                              "",
-                        ),
-                      ),
-                    ),
+                    currentState.printableStateShort
                   ),
                 ),
               ),
             ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Text(
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Color(0xffcccccc),
+                      ),
+                      currentState.printableStateLong
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: const Color(0xff333333),
+              ),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 410,
+                ),
+                child: SingleChildScrollView(
+                  reverse: true,
+                  child: SelectableText(
+                    style: const TextStyle(
+                      fontFamily: "Courier New",
+                    ),
+                    // filter for log messages in the message queue and print those out to this console display
+                    HarvesterIsolateSet.instance
+                        .get(widget.digest)
+                        ?.messageQueue
+                        .where((message) {
+                      return message.type == HarvesterIsolateMessageType.log;
+                    })
+                        .map((e) => e.data.toString())
+                        .join("\n") ??
+                        "",
+                  ),
+                ),
+              ),
+            ),
           ),
           Row(
             children: [
@@ -94,7 +127,8 @@ class _IsolateCardState extends State<IsolateCard> {
                 ]).any((e) => currentState == e)
                     ? () {
                         HarvesterIsolateSet.instance.get(widget.digest)?.sendPort.send(HarvesterIsolateMessage(HarvesterIsolateMessageType.start));
-                      } : null,
+                      }
+                    : null,
                 child: const Text("Start"),
               ),
               TextButton(
@@ -103,8 +137,25 @@ class _IsolateCardState extends State<IsolateCard> {
                 ]).any((e) => currentState == e)
                     ? () {
                         HarvesterIsolateSet.instance.get(widget.digest)?.sendPort.send(HarvesterIsolateMessage(HarvesterIsolateMessageType.stop));
-                      } : null,
+                      }
+                    : null,
                 child: const Text("Stop"),
+              ),
+              TextButton(
+                onPressed: ([
+                  HarvesterIsolateState.stopped,
+                  HarvesterIsolateState.crashed
+                ]).any((e) => currentState == e) ? () {
+                  HarvesterIsolate? isolate = HarvesterIsolateSet.instance.get(widget.digest);
+                  if (isolate != null) {
+                    isolate.isolate.kill(priority: 0);
+                  }
+                  // TODO: fix removing cards with index 0, index 1 inherits state from index 0 card
+                  setState(() {
+                    HarvesterIsolateSet.instance.set.remove(isolate);
+                  });
+                } : null,
+                child: const Text("Remove"),
               ),
             ],
           ),
