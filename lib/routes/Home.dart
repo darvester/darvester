ccf339a2e40b2565beff0cf8a34f46c4301b5281
V3ntus <29584664+V3ntus@users.noreply.github.com>
2023-04-02 10:09:44 -0500

    feat: add isolate pie chart to home dashboard

--- lib/routes/Home.dart
+++ lib/routes/Home.dart
@@ -1,3 +1,6 @@
+import 'package:darvester/darvester/harvester_isolate.dart';
+import 'package:darvester/darvester/isolate_message.dart';
+import 'package:darvester/darvester/isolate_set.dart';
 import 'package:darvester/database.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart' show PieChart, PieChartData, PieChartSectionData;
@@ -34,6 +37,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    Set<HarvesterIsolate> isolateSet = HarvesterIsolateSet.instance.set;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey.shade900,
@@ -187,8 +191,81 @@ class _HomeState extends State<Home> {
               ],
             ),
             Column(
-              children: const [
-                Placeholder(),
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 400,
+                    maxWidth: MediaQuery.of(context).size.width / 2,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xff2a2a2a),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    margin: const EdgeInsets.all(16),
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Expanded(
+                              child: Text(
+                                "Isolates",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        (isolateSet.length > 0)
+                            ? ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  maxHeight: 200,
+                                  maxWidth: MediaQuery.of(context).size.width / 2,
+                                ),
+                                child: PieChart(
+                                  PieChartData(
+                                    sections: [
+                                      PieChartSectionData(
+                                        value: isolateSet.length.toDouble(),
+                                        title: "Total",
+                                      ),
+                                      ...(isolateSet.where((element) => element.state == HarvesterIsolateState.started).isNotEmpty
+                                          ? [
+                                              PieChartSectionData(
+                                                value: isolateSet.where((element) => element.state == HarvesterIsolateState.started).length.toDouble(),
+                                                title: HarvesterIsolateState.started.printableStateShort,
+                                              )
+                                            ]
+                                          : []),
+                                      ...(isolateSet.where((element) => element.state == HarvesterIsolateState.stopped).isNotEmpty
+                                          ? [
+                                              PieChartSectionData(
+                                                value: isolateSet.where((element) => element.state == HarvesterIsolateState.stopped).length.toDouble(),
+                                                title: HarvesterIsolateState.stopped.printableStateShort,
+                                              )
+                                            ]
+                                          : []),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : const Expanded(
+                                child: Center(
+                                  child: Text(
+                                    "No isolates spawned",
+                                    style: TextStyle(
+                                      color: Color(0xff777777),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ],
